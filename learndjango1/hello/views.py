ce7d65b6f2655ff0985bd01d83ab9d6c70ec3f76
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-01-16 15:48:38 +0530

    rendering html files

--- learndjango1/hello/views.py
+++ learndjango1/hello/views.py
@@ -5,8 +5,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 
 def thasa(request):
     return HttpResponse("Hello, Thasa!")
+
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
