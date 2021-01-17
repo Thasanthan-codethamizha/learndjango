fadc867727c6c6caa71c8855b366aade9f13cce5
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-01-17 10:51:04 +0530

    todoapp

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -7,15 +7,11 @@ from django.urls import reverse
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
+    numberf = forms.IntegerField(label="numbers", min_value=1, max_value=5)
 
 
 def index(request):
-
-    # Check if there already exists a "tasks" key in our session
-
     if "tasks" not in request.session:
-
-        # If not, create a new list
         request.session["tasks"] = []
 
     return render(request, "tasks/index.html", {
@@ -27,28 +23,15 @@ def index(request):
 
 def add(request):
     if request.method == "POST":
-
-        # Take in the data the user submitted and save it as form
         form = NewTaskForm(request.POST)
-
-        # Check if form data is valid (server-side)
         if form.is_valid():
-
-            # Isolate the task from the 'cleaned' version of form data
             task = form.cleaned_data["task"]
-
-            # Add the new task to our list of tasks
             request.session["tasks"] += [task]
-
-            # Redirect user to list of tasks
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
-
-            # If the form is invalid, re-render the page with existing information.
             return render(request, "tasks/add.html", {
                 "form": form
             })
-
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
