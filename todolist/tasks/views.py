3bb7f2c6ffa59179437221b24e2d44663bbe0d92
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-01-17 06:54:23 +0530

    todoapp

---
+++ todolist/tasks/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+tasks = ["foo", "bar", "baz"]
+
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+
+
+def add(request):
+    return render(request, "tasks/add.html")
