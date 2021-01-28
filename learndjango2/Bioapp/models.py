c4f354e68cd64d880ca85c4a465f61451e057120
Thasanthan-codeThamizha <sivakumarthasanthan@gmail.com>
2021-01-28 17:31:32 +0530

    learndjango2

--- learndjango2/Bioapp/models.py
+++ learndjango2/Bioapp/models.py
@@ -7,3 +7,7 @@ class Bio(models.Model):
     name = models.CharField(max_length=150)
     age = models.IntegerField()
     email = models.EmailField()
+    profile_pic = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- learndjango2/Bioapp/views.py
+++ learndjango2/Bioapp/views.py
@@ -9,3 +9,15 @@ def index(request):
     return render(request, "index.html", {
         'users': users,
     })
+
+
+def add(request):
+    if request.method == "POST":
+        if request.POST.get('name') and request.POST.get('age') and request.POST.get('email'):
+            userform = Bio()
+            userform.name = request.POST.get('name')
+            userform.age = request.POST.get('age')
+            userform.email = request.POST.get('email')
+            userform.profile_pic = request.POST.get('image')
+            userform.save()
+    return render(request, "add.html")
